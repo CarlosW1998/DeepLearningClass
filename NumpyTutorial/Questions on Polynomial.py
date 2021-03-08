e176a5d102cc2ec5631195b13c562cc376186477
Carlos <you@example.com>
2021-03-08 17:32:56 -0300

    numpy questions

---
+++ NumpyTutorial/Questions on Polynomial.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+#Questions on Polynomial
+# Define a polynomial function
+# How to add one polynomial to another using NumPy in Python?
+# How to subtract one polynomial to another using NumPy in Python?
+# How to multiply a polynomial to another using NumPy in Python?
+# How to divide a polynomial to another using NumPy in Python?
+# Find the roots of the polynomials using NumPy
+# Evaluate a 2-D polynomial series on the Cartesian product
+# Evaluate a 3-D polynomial series on the Cartesian product
